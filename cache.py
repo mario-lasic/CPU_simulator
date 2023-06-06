5f61ab4949f2f54de7ea8b8fa2e9733e798d1b7b
Mario Lasić <mario.lasic@outlook.com>
2023-06-06 12:24:25 +0200

    Defined function to search cache

--- cache.py
+++ cache.py
@@ -5,4 +5,16 @@ SIZE = 16
 class Cache:
     def __init__(self) -> None:
         self.cache = collections.deque(maxlen = SIZE)
+        self.cache_flush()
         
+
+def cache_flush(self):
+    for i in range(SIZE):
+        self.cache.append(("", ""))
+
+def cache_search(self, address):
+    for i in len(self.cache):
+        if self.cache[i][0] == address:
+            return self.cache[i][1]
+    return None 
+
